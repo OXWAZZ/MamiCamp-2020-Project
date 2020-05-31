8591847dbd2cf2f05a23fd7233af1a4ebd27c314
OXWAZZ <muhammad.rahmahalim@gmail.com>
2020-05-31 18:53:03 +0700

    Update Course 9 Eps 11

--- birdboard/resources/views/projects/show.blade.php
+++ birdboard/resources/views/projects/show.blade.php
@@ -1,7 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
-    <a href="/projects">Go Back</a>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-gray text-sm font-normal">
+                <a href="/projects" class="no-underline text-gray">My Projects</a> / {{ $project->title}}
+            </h2>
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="lg:flex -mx-3" style="box-sizing: border-box;">
+
+            <div class="lg:w-3/4 px-3 mb-8">
+                <div class="mb-6">
+                    <h2 class="text-lg text-gray font-normal">Tasks</h2>
+                    <div class="card mb-6">Lorem Ipsum</div>
+                    <div class="card mb-6">Lorem Ipsum</div>
+                    <div class="card mb-6">Lorem Ipsum</div>
+                    <div class="card">Lorem Ipsum</div>
+                </div>
+
+                <div>
+                    <h2 class="text-lg text-gray font-normal">General Notes</h2>
+                    <textarea class="card w-full " style="min-height: 200px; max-width: 900px;">Lorem Ipsum</textarea>
+                </div>            
+            </div>
+
+            <div class="lg:w-1/4 px-3">
+                @include('projects.card')
+            </div>
+
+        </div>
+    
+
+    </main>
+
+    
 @endsection
